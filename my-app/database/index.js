73b2ba170726fe41590314255d3cc6d6044c3d60
hassenbarred <hassenelbrd@gmail.com>
2024-02-12 11:24:13 +0100

    hassen part

--- my-app/database/index.js
+++ my-app/database/index.js
@@ -27,5 +27,39 @@ const postjob=(obj)=>{
   const sql=' insert into post SET ?  '
   return connection.query(sql,obj)
 }
-module.exports = {getallworker,getoneclient,postjob}
+
+//Singup for Client & Employer
+const SignUpe =(value,callback)=>{
+  const sql = "INSERT INTO employer SET ? "
+  connection.query(sql,[value],function(err,results){
+    callback(err,results)
+  })
+}
+const SignUpc =(value)=>{
+  const sql = "INSERT INTO client SET ?"
+ return  connection.query(sql,value)}
+
+ //SignIn for Client &  Employer
+
+ const SignInc =(value)=>{
+  const sql = "SELECT * FROM client WHERE `emailc`=?  AND  `passwordc`=?"
+ return  connection.query(sql,[value.email,value.password])}
+
+ const SignIne =(value)=>{
+  const sql = "SELECT * FROM employer WHERE `emaile`=?  AND  `passworde`=?"
+ return  connection.query(sql,[value.email,value.password])}
+
+//  Getting All  Data from Client & Employer
+ const getallc=()=>{
+  const sql= "SELECT * FROM client"
+  return connection.query(sql)
+ }
+ const getalle=()=>{
+  const sql= "SELECT * FROM employer"
+  return connection.query(sql)
+ }
+
+module.exports={getalle,SignIne,SignInc,SignUpe,SignUpc,getallc , getallworker,getoneclient,postjob};
+
+
 
